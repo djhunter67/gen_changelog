399be870787804cd7e0f87e6846eefbeb4aa33c4
Christerpher Hunter <djhunter67@gmail.com>
2023-12-13 11:42:22 -0500

    feat: get the keyword for the commit message

--- src/main.py
+++ src/main.py
@@ -6,16 +6,12 @@ and write it to the file CHANGELOG.md
 """
 
 
-import os
-import sys
+import pathlib as path
 import subprocess
-import re
 import datetime
-import argparse
+from colorama import Fore as F
 
-
-def get_current_branch():
-    """Get the current git branch"""
+R = F.RESET
 
 
 def get_current_date():
@@ -28,9 +24,9 @@ class GitData:
 
     def __str__(self):
         return "GitData: branch: %s, tag: %s, commit: %s" % (
-            self.branch,
-            self.tag,
-            self.commit
+            str(self.branch),
+            str(self.tag),
+            str(self.commit)
         )
 
     def get_current_branch(self):
@@ -50,3 +46,89 @@ class GitData:
         return subprocess.check_output(
             ['git', 'rev-parse', '--short', 'HEAD']
         ).strip()
+
+    def get_last_commit_msg(self):
+        """Get the last commit message"""
+        return subprocess.check_output(
+            ['git', 'log', '-1', '--pretty=%B']
+        ).strip()
+
+    def get_msg_keywords(self):
+        """Get the commit message keywords indicated by the end of a colon"""
+        keys = self.get_last_commit_msg().split(b':')[0]
+
+        return keys.decode('utf-8')
+
+
+class ChangelogTemplate:
+
+    def __repr__(self):
+        return """
+# CHANGELOG
+
+Author: Hunter, Christerpher
+
+All notable changes will be appended here.
+
+This project, henceforth, will recongnize [semantic versioning](https://semver.org/).
+
+## [⭐.✴️.✳️] - YEAR MONTH DAY
+
+Here we write upgrade and change notes.
+
+⭐ MAJOR version when you make incompatible API changes,
+
+✴️ MINOR version when you add functionality in a backwards compatible manner
+
+✳️ PATCH version when you make backwards compatible bug fixes.
+
+--------------------------------------
+
+"""
+
+
+class GenerateChangelog:
+
+    def __init__(self):
+        pass
+
+    def get_changelog(self):
+        """Look for the CHANGELOG.md file and return it's contents"""
+        if path.exists('CHANGELOG.md'):
+            with open('CHANGELOG.md', 'r') as file:
+                return file.read()
+        else:
+            return None
+
+    def write_changelog(self, changelog):
+
+        with open('CHANGELOG.md', 'w') as file:
+            file.write(changelog)
+
+    def get_current_version(self, changelog):
+        """Get the current version from the changelog"""
+        match = re.search(r'## \[(\d+.\d+.\d+)\]', changelog)
+        if match:
+            return match.group(1)
+        else:
+            return None
+
+    def get_next_version(self, current_version):
+        """Get the next version"""
+        if current_version:
+            version = current_version.split('.')
+            version[-1] = str(int(version[-1]) + 1)
+            return '.'.join(version)
+        else:
+            return None
+
+
+def main():
+
+    git_data = GitData()
+
+    print(f"\n{F.GREEN}git_data{R}: {git_data.get_msg_keywords()}\n")
+
+
+if '__main__' == __name__:
+    main()
